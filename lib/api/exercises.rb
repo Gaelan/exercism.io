da2e6a7836e88cb5903be97bec41d72dd7a166db
Gaelan Steele <gbs@canishe.com>
2014-04-03 07:35:07 -0700

    DRY exercises API.

--- lib/api/exercises.rb
+++ lib/api/exercises.rb
@@ -2,14 +2,16 @@ require 'api/exercises/homework'
 
 class ExercismAPI < Sinatra::Base
   get '/exercises' do
-    require_user
-    content_type 'application/json', :charset => 'utf-8'
-    Homework.new(current_user).all.to_json
+    get_exercises :all
   end
 
   get '/exercises/current' do
+    get_exercises :current
+  end
+
+  def get_exercises(type)
     require_user
     content_type 'application/json', :charset => 'utf-8'
-    Homework.new(current_user).current.to_json
+    Homework.new(current_user).send(type).to_json
   end
 end
